6335653d6f247fcc9adeb55cc3dd19ce46ebd8a9
Connor Krupp <connorkrupp@icloud.com>
2015-09-18 16:34:34 -0400

    Beautified all files

--- source/library/post-formats/format-link.php
+++ source/library/post-formats/format-link.php
@@ -7,11 +7,11 @@
                   <h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 
                   <p class="byline vcard">
-                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+                    <?php printf(__('Posted', 'bonestheme').' %1$s %2$s',
                        /* the time the post was published */
-                       '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+                       '<time class="updated entry-time" datetime="'.get_the_time('Y-m-d').'" itemprop="datePublished">'.get_the_time(get_option('date_format')).'</time>',
                        /* the author of the post */
-                       '<span class="by">'.__( 'by', 'bonestheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                       '<span class="by">'.__('by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">'.get_the_author_link(get_the_author_meta('ID')).'</span>'
                     ); ?>
                   </p>
 
@@ -34,20 +34,20 @@
                      * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
                      *
                     */
-                    wp_link_pages( array(
-                      'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-                      'after'       => '</div>',
+                    wp_link_pages(array(
+                      'before' => '<div class="page-links"><span class="page-links-title">'.__('Pages:', 'bonestheme').'</span>',
+                      'after' => '</div>',
                       'link_before' => '<span>',
-                      'link_after'  => '</span>',
-                    ) );
+                      'link_after' => '</span>',
+                    ));
                   ?>
                 </section> <?php // end article section ?>
 
                 <footer class="article-footer">
 
-                  <?php printf( __( 'Filed under: %1$s', 'bonestheme' ), get_the_category_list(', ') ); ?>
+                  <?php printf(__('Filed under: %1$s', 'bonestheme'), get_the_category_list(', ')); ?>
 
-                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+                  <?php the_tags('<p class="tags"><span class="tags-title">'.__('Tags:', 'bonestheme').'</span> ', ', ', '</p>'); ?>
 
                 </footer> <?php // end article footer ?>
 
--- source/pages/404.php
+++ source/pages/404.php
@@ -4,13 +4,13 @@
     <div id="inner-content" class="wrap cf">
       <main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
         <article id="post-not-found" class="hentry cf">
-        
+
           <header class="article-header">
-            <h1><?php _e( 'Epic 404 - Article Not Found', 'bonestheme' ); ?></h1>
+            <h1><?php _e('Epic 404 - Article Not Found', 'bonestheme'); ?></h1>
           </header>
 
           <section class="entry-content">
-            <p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
+            <p><?php _e('The article you were looking for was not found, but maybe try looking again!', 'bonestheme'); ?></p>
           </section>
 
           <section class="search">
@@ -18,7 +18,7 @@
           </section>
 
           <footer class="article-footer">
-            <p><?php _e( 'This is the 404.php template.', 'bonestheme' ); ?></p>
+            <p><?php _e('This is the 404.php template.', 'bonestheme'); ?></p>
           </footer>
 
         </article>
